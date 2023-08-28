3cb910f2dd34a442590b140f399f2ec459fc204d
rahuly29 <yadavrahul3298@gmail.com>
2023-08-28 12:51:53 +0530

    first commit

---
+++ src/assets/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReview from "./CustomerReview";
+import Subscribe from './Subscribe'
+
+export{
+    Hero,
+    PopularProduct,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Footer,
+    CustomerReview,
+    Subscribe
+}
